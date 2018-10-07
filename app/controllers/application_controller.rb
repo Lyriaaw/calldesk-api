2e97ef8f87e2ce18294c07b237568057f0128fd4
Amalric de Buffières <amalric.debuffieres@gmail.com>
2018-10-07 22:38:52 +0200

    Added root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
+  def index
+    render json: {success: true, server: 'started'}
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'ask', to: 'dialog#ask'
   get 'drink', to: 'dialog#search_by_drink'
   get 'recipe', to: 'dialog#get_recipe'
+  root to: 'application#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
